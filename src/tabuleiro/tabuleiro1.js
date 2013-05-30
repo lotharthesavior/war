283e8170cfa1d898776b4f868f45c02ec62f709c
lotharthesavior <lotharthesavior@gmail.com>
2013-05-30 12:47:51 -0300

    início das atvidades

---
+++ src/tabuleiro/tabuleiro1.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+	
+	// variaveis
+
+	$('#tabuleiro').html( estrutura );
+
+	$.each( linhas, function( index, value ){
+		$('#tabuleiro').append( value );
+	});
+
+});
