3a6cd5351d90b69446793e2fdc04af48ad58ac21
Arthur Almeida <arthur.almeidapereira@gmail.com>
2013-05-30 18:29:30 -0300

    refactoring

--- src/model/tabuleiro.js
+++ src/model/tabuleiro.js
@@ -1,8 +1,34 @@
-
-function Tabuleiro(){
-	this.territorios = [];
+ /**
+  * Armazena as informações referentes ao tabuleiro
+  *
+  * @class Tabuleiro
+  * @constructor
+  * @param {String} nome Nome do argumento
+  * @param {Object} tamanho Objeto contendo as propriedades de linha e coluna
+  */
+function Tabuleiro(nome, tamanho){
+	this.nome = nome;
+	this.tamanho = tamanho;
 }
 
-Tabuleiro.prototype.addTerritorio = function(territorio) {
-	this.territorios.push(territorio);
-}
+/**
+* Retorna o nome do tabuleiro
+*
+* @method getNome
+* @return {Strine} Retorna o nome do tabuleiro
+*/
+Tabuleiro.prototype.getNome = function() {
+	return this.nome;	
+};
+
+
+/**
+* Retorna o tamanho do tabuleiro
+*
+* @method getTamanho
+* @return {Strine} Retorna o tamanho do tabuleiro
+*/
+Tabuleiro.prototype.getTamanho = function() {
+	return this.tamanho;
+};
+
---
+++ src/tabuleiro/tabuleiro_view.js
@@ -0,0 +1,75 @@
+
+ /**
+  * Renderiza o tabuleiro em HTML
+  *
+  * @class TabuleiroView
+  * @constructor
+  * @param {Function} tabuleiro Tabuleiro que deve ser renderizado
+  */
+function TabuleiroView( tabuleiro ){
+	this.tabuleiro 			= tabuleiro;
+	this.celulaSelecionada 	= false;
+}
+
+TabuleiroView.prototype.render = function(container) {
+	
+	// variaveis
+	var nomeTabuleiro 		= this.tabuleiro.getNome(),
+		linhasTabuleiro 	= this.tabuleiro.getTamanho().linha,
+		colunasTabuleiro 	= this.tabuleiro.getTamanho().coluna,
+		grade 				= []
+
+		// utilitarios
+		contador 	 = 1,
+		coluna_atual = '',
+		linha_atual	 = '',
+
+		// linhasTabuleiro
+		linha = $('<div class="linha"></div>'),
+
+		// colunasTabuleiro
+		coluna = $('<div class="coluna celula_desselecionada"></div>');
+
+	// montagem
+	for( var i = 1; i <= linhasTabuleiro; i++ ){  // aplica linhasTabuleiro
+		linha_atual = linha.clone();
+		linha_atual.attr('id','linha_'+i+'_'+nomeTabuleiro);
+		grade.push( linha_atual );
+	};
+
+	// aplica colunasTabuleiro
+	$.each( grade, function( index, value ){ 
+
+		for( i = 1; i <= colunasTabuleiro; i++ ){
+			coluna_atual = coluna.clone();
+			coluna_atual.attr('id','coluna_'+i+'_linha'+contador+'_'+nomeTabuleiro);
+			value.append( coluna_atual );
+		}
+
+		contador++;
+
+	});
+
+	$(container).html( grade );
+	this.aplicaEventos();
+};
+
+TabuleiroView.prototype.aplicaEventos = function() {
+
+	var that = this;
+	$( '.coluna' ).click(function(){			
+		that.desselecionaCelula();
+		that.aplicaEstiloParaCelulasAtacaveis();
+		that.celulaSelecionada = this;
+		$( that.celulaSelecionada ).removeClass('celula_desselecionada');// remove class
+		$( that.celulaSelecionada ).addClass('celula_selecionada'); // add class
+	});
+};
+
+TabuleiroView.prototype.desselecionaCelula = function() {
+	$( this.celulaSelecionada ).removeClass('celula_selecionada');// remove class
+	$( this.celulaSelecionada ).addClass('celula_desselecionada');// remove class
+	this.celulaSelecionada = false;
+};
+
+TabuleiroView.prototype.aplicaEstiloParaCelulasAtacaveis = function() {};	
